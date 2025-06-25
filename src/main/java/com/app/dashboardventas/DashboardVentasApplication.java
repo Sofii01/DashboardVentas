c3c589202cef90d8fcd88ed1f7a6a31d21154d61
Sofii01 <sofiibritos01@gmail.com>
2025-06-25 15:38:16 -0300

    feat: project setup with base structure

---
+++ src/main/java/com/app/dashboardventas/DashboardVentasApplication.java
@@ -0,0 +1,13 @@
+package com.app.dashboardventas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashboardVentasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DashboardVentasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/dashboardventas/DashboardVentasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.dashboardventas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashboardVentasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
